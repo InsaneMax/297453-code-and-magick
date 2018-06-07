741dd991489a330dc32068358360e2dc859c4770
Maxim Tantsura <tm_4@mail.ru>
2018-06-07 09:01:07 +0300

    цикл for

--- js/stat.js
+++ js/stat.js
@@ -22,29 +22,38 @@ window.renderStatistics = function (ctx, names, times) {
 
   renderBlock(ctx, CLOUD_X, CLOUD_Y, 'rgba(0, 0, 0, 0.7)');
 
+  ctx.fillStyle = '#000000';
 
-  names.forEach(function (name, index) {
-    ctx.fillStyle = '#000000';
-    ctx.font = '16px Pt Mono';
-    if (name === 'Вы') {
-      ctx.fillStyle = '#0000ff';
+  for (var i = 0; i < names.length; i++) {
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'red';
     }
-    console.log(name, times[index]);
-  });
+    console.log(names[i]);
+  }
+
+
+  // names.forEach(function (name, index) {
+  //   ctx.fillStyle = '#000000';
+  //   ctx.font = '16px Pt Mono';
+  //   if (name === 'Вы') {
+  //     ctx.fillStyle = '#0000ff';
+  //   }
+  //   console.log(name, times[index]);
+  // });
 
   ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP * 2);
   ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + FONT_GAP + GAP * 3);
 
-  ctx.fillText(names, CLOUD_X + GAP, CLOUD_HEIGHT - GAP);
+  ctx.fillText(names[0], CLOUD_X + GAP, CLOUD_HEIGHT - GAP);
   ctx.fillRect(CLOUD_X + GAP, (GAP * 3) + FONT_GAP * 2 + CLOUD_Y, BAR_WIDTH, BAR_HEIGHT);
 
-  ctx.fillText(names, CLOUD_X + GAP + TEXT_WIDTH + GAP, CLOUD_HEIGHT - GAP);
+  ctx.fillText(names[1], CLOUD_X + GAP + TEXT_WIDTH + GAP, CLOUD_HEIGHT - GAP);
   ctx.fillRect(CLOUD_X + GAP + BAR_WIDTH + BAR_GAP, (GAP * 3) + FONT_GAP * 2 + CLOUD_Y, BAR_WIDTH, BAR_HEIGHT);
 
-  ctx.fillText(names, CLOUD_X + GAP + (TEXT_WIDTH + GAP) * 2, CLOUD_HEIGHT - GAP);
+  ctx.fillText(names[2], CLOUD_X + GAP + (TEXT_WIDTH + GAP) * 2, CLOUD_HEIGHT - GAP);
   ctx.fillRect(CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP) * 2, (GAP * 3) + FONT_GAP * 2 + CLOUD_Y, BAR_WIDTH, BAR_HEIGHT);
 
-  ctx.fillText(names, CLOUD_X + GAP + (TEXT_WIDTH + GAP) * 3, CLOUD_HEIGHT - GAP);
+  ctx.fillText(names[3], CLOUD_X + GAP + (TEXT_WIDTH + GAP) * 3, CLOUD_HEIGHT - GAP);
   ctx.fillRect(CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP) * 3, (GAP * 3) + FONT_GAP * 2 + CLOUD_Y, BAR_WIDTH, BAR_HEIGHT);
 };
 
