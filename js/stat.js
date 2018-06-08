d8fb689fab05454e03d0d580149e955704319422
Maxim Tantsura <tm_4@mail.ru>
2018-06-09 00:05:33 +0300

    блок статистики готов

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
@@ -9,9 +8,9 @@ var GAP = 10;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, BLOCK_WIDTH, BLOCK_HEIGHT);
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
   ctx.fillStyle = '#ffffff';
-  ctx.fillRect(x - GAP, y - GAP, BLOCK_WIDTH, BLOCK_HEIGHT);
+  ctx.fillRect(x - GAP, y - GAP, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
 var getMaxOfArray = function (nameOfArray) {
@@ -26,7 +25,6 @@ window.renderStatistics = function (ctx, names, times) {
   var TEXT_WIDTH = 80;
   var barY = CLOUD_HEIGHT - GAP * 4 - FONT_GAP * 3;
   var maxValue = getMaxOfArray(times);
-  var randomNumber = Math.floor(Math.random() * 255);
 
   var getRandomBlue = function () {
     return 'rgb(' + '0, 0, ' + randomNumber + ')';
@@ -34,17 +32,17 @@ window.renderStatistics = function (ctx, names, times) {
 
   renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgba(0, 0, 0, 0.7)');
 
-  ctx.fillStyle = '#000000';
   ctx.font = '16px PT Mono';
+  ctx.fillStyle = '#000000';
   ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 4, CLOUD_Y + GAP * 2);
   ctx.fillText('Список результатов:', CLOUD_X + GAP * 4, CLOUD_Y + FONT_GAP + GAP * 3);
 
   for (var i = 0; i < names.length; i++) {
+    var randomNumber = Math.floor(Math.random() * 255);
     times[i] = Math.floor(times[i]);
+    ctx.fillStyle = getRandomBlue();
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'red';
-    } else {
-      ctx.fillStyle = getRandomBlue();
     }
     ctx.fillText(names[i], CLOUD_X + GAP * 3 + (TEXT_WIDTH + GAP) * i, CLOUD_HEIGHT - GAP);
     ctx.fillRect(CLOUD_X + GAP * 3 + (BAR_WIDTH + BAR_GAP) * i, (GAP * 3) + FONT_GAP * 2 + barY, BAR_WIDTH, BAR_HEIGHT * times[i] * -1 / maxValue);
